2ebef292ae473a320205008bd1e074f278d9eac6
Vlad <100029255+Bugs-Production@users.noreply.github.com>
2024-10-29 23:11:06 +0600

    Настройка работы с БД (#5)
    * настроил alembic и sqlalchemy

--- notification_api/src/core/config.py
+++ notification_api/src/core/config.py
@@ -10,6 +10,10 @@ class Settings(BaseSettings):
     )
     project_name: str = Field("notification_api", alias="PROJECT_NAME")
     broker_url: str = Field("amqp://user:password@rabbitmq:5672/", alias="BROKER_URL")
+    postgres_url: str = Field(
+        "postgresql+asyncpg://postgres:postgres@db:5432/foo", alias="POSTGRES_URL"
+    )
+    engine_echo: bool = Field(default=False, alias="ENGINE_ECHO")
 
 
 settings = Settings()
