8a3904cae6583810d5a2540f9c47b812c8a08e28
vtitoff <v.titoff2010@yandex.ru>
2024-11-08 02:44:31 +0300

    Создавать таск на массовую рассылку череез шедулер

--- notification_api/src/tasks/get_admin_notifications.py
+++ notification_api/src/tasks/get_admin_notifications.py
@@ -4,8 +4,17 @@ from celery import shared_task
 from db.sync_postgres import get_sync_session
 from models.admin import NotificationTask, NotificationTaskStatusEnum
 from sqlalchemy import func, select
+from tasks.send_notification import send_mass_email
 
 logger = logging.getLogger()
+
+
+def update_task_status(
+    task: NotificationTask, status: NotificationTaskStatusEnum, session
+):
+    task.status = status
+    task.updated_at = func.now()
+    session.commit()
 
 
 @shared_task
@@ -16,8 +25,12 @@ def get_admin_notifications() -> None:
             NotificationTask.status == NotificationTaskStatusEnum.INIT
         )
         result = session.scalars(stmt)
+
         for row in result.all():
-            row.status = NotificationTaskStatusEnum.IN_PROGRESS
-            row.updated_at = func.now()
-            session.commit()
-            logger.info(row)
+            try:
+                update_task_status(row, NotificationTaskStatusEnum.IN_PROGRESS, session)
+                send_mass_email.delay(event_type=row.type.value, notification_data=row.filter)
+                update_task_status(row, NotificationTaskStatusEnum.SUCCESS, session)
+            except Exception as e:
+                logger.error(e)
+                update_task_status(row, NotificationTaskStatusEnum.FAILED, session)
